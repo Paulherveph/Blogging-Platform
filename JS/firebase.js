6f949fd2744a38fb8e65177b49d31bf77e6dd6f3
TATIEZE PAUL HERVE <150878556+Paulherveph@users.noreply.github.com>
2024-05-13 14:50:03 +0100

    Update firebase.js

--- JS/firebase.js
+++ JS/firebase.js
@@ -1,13 +1,13 @@
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js';
-import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.11.0/firebase-firestore.js';
+import { initializeApp } from './firebase-app.js';
+import { getFirestore } from './firebase-firestore.js';
 
-const firebaseConfig = { 
-  apiKey : "AIzaSyBnXqLohscuhz74tTl4XXRYxBJFrg8xxnM" , 
-  authDomain : "blogging-website-48c92.firebaseapp.com" , 
-  projectId : "blogging-website-48c92" , 
-  storageBucket : "blogging-website-48c92.appspot.com" , 
-  messagingSenderId : "1091336465795" , 
-  appId : "1:1091336465795:web:2605574cf7d90600a8dab6" 
+const firebaseConfig = {
+  apiKey: "your-api-key",
+  authDomain: "your-auth-domain",
+  projectId: "your-project-id",
+  storageBucket: "your-storage-bucket",
+  messagingSenderId: "your-messaging-sender-id",
+  appId: "your-app-id"
 };
 
 // Initialize Firebase
