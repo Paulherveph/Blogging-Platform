bde7f04867a21b26445b0e77099dae2d36e229b4
TATIEZE PAUL HERVE <150878556+Paulherveph@users.noreply.github.com>
2024-05-17 05:18:05 +0100

    Update home.js

--- JS/home.js
+++ JS/home.js
@@ -2,20 +2,20 @@ const blogSection = document.querySelector('.blogs-section');
 
 db.collection("blogs").get().then((blogs) => {
   blogs.forEach(blog => {
-    if(blog.id != decodeURI(location.pathname.split("/").pop())){
+    if (blog.id !== decodeURI(location.pathname.split("/").pop())) {
       createBlog(blog);
     }
   });
-})
+}).catch(error => console.error('Error fetching blogs:', error));
 
-const createBlog = (blob) => {
+const createBlog = (blog) => {
   let data = blog.data();
   blogSection.innerHTML += `
-  <div class="blog-card">
-  <img src="${data.banner.Image}" class="blog-image" alt="">
-  <h1 class="blog-title">${data.title.substring(0, 100) + '...'}</h1>
-  <p class="blog-overview">${data.article.substring(0,200) + '...'}</p>
+    <div class="blog-card">
+      <img src="${data.bannerImage}" class="blog-image" alt="Blog Banner">
+      <h1 class="blog-title">${data.title.substring(0, 100)}...</h1>
+      <p class="blog-overview">${data.article.substring(0, 200)}...</p>
       <a href="/${blog.id}" class="btn dark">read</a>
- </div> 
-  `
-}
+    </div>
+  `;
+};
