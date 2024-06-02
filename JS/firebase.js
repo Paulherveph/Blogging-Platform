a99facffb2de3b9320455d418f567ce73a23c48b
TATIEZE PAUL HERVE <150878556+Paulherveph@users.noreply.github.com>
2024-06-02 11:01:12 +0100

    firebase.js

--- JS/firebase.js
+++ JS/firebase.js
@@ -1,17 +1,26 @@
-import { initializeApp } from './firebase-app.js';
-import { getFirestore } from './firebase-firestore.js';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getFirestore, collection, doc, getDoc } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+import { getStorage, ref } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-storage.js";
+import { getAuth, signOut } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+
 
 const firebaseConfig = {
-  apiKey: "your-api-key",
-  authDomain: "your-auth-domain",
-  projectId: "your-project-id",
-  storageBucket: "your-storage-bucket",
-  messagingSenderId: "your-messaging-sender-id",
-  appId: "your-app-id"
+  apiKey: "api-key",
+  authDomain: "auth-domain",
+  projectId: "project-id",
+  storageBucket: "storage-bucket",
+  messagingSenderId: "messaging-sender-id",
+  appId: "app-id"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+const storage = getStorage(app);
+const auth = getAuth(app);
 
-export { db };
+// References to storage paths
+const imagesRef = ref(storage, 'images');
+
+export { db, imagesRef, auth, collection, doc, getDoc };
