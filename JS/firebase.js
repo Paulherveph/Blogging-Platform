8e4580cbc346ac95168ad09f82ca9bff2f7642ac
TATIEZE PAUL HERVE <150878556+Paulherveph@users.noreply.github.com>
2024-06-15 11:43:49 +0100

    firebase.js

--- JS/firebase.js
+++ JS/firebase.js
@@ -1,17 +1,20 @@
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-import { getFirestore, collection, doc, getDoc } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+import { getFirestore, collection, doc, getDoc, getDocs } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 import { getStorage, ref } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-storage.js";
-import { getAuth, signOut } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+import { getAuth, GoogleAuthProvider, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+import {  query, where,  deleteDoc } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "auth-domain",
-  projectId: "project-id",
-  storageBucket: "storage-bucket",
-  messagingSenderId: "messaging-sender-id",
-  appId: "app-id"
+  apiKey: "AIzaSyCenT_6GCkDOtnh8ZuTzVFoBKm5ZkaVouc",
+  authDomain: "blogging-website-46277 .firebaseapp.com",
+  projectId: "blogging-website-46277",
+  storageBucket: "blogging-website-46277.appspot.com",
+  messagingSenderId: "78510713062",
+  appId: "1:78510713062:web:ba62029ae5f5609f827327"
 };
 
 // Initialize Firebase
@@ -19,8 +22,11 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const storage = getStorage(app);
 const auth = getAuth(app);
+auth.languageCode = 'it';
+const provider = new  GoogleAuthProvider();
 
 // References to storage paths
 const imagesRef = ref(storage, 'images');
 
-export { db, imagesRef, auth, collection, doc, getDoc };
+export { db, imagesRef, auth, collection, doc, getDoc, getDocs,  provider, createUserWithEmailAndPassword, signInWithEmailAndPassword, query, where,  deleteDoc };
+
