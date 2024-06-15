b1f7bc2a78203eff527a6d1fdf5d12c6503d37ee
TATIEZE PAUL HERVE <150878556+Paulherveph@users.noreply.github.com>
2024-06-15 10:09:59 +0100

    nav.js

--- JS/nav.js
+++ JS/nav.js
@@ -1,1 +1,27 @@
+import { auth } from './firebase.js'; 
 
+document.addEventListener('DOMContentLoaded', function () {
+  let ul = document.querySelector('.links-container');
+
+  auth.onAuthStateChanged((user) => { 
+    if (user) {
+      // User is logged in
+      ul.innerHTML += `
+        <li class="link-item"><a href="/admin" class="link">Dashboard</a></li>
+      `;
+    } else {
+      // No user is logged in
+      ul.innerHTML += `
+        <li class="link-item"><a href="/login" class="link">Login</a></li>
+      `;
+    }
+  });
+});
+
+function LogoutUser() {
+  auth.signOut().then(() => {
+    location.reload();
+  }).catch((error) => {
+    console.error('Error signing out:', error);
+  });
+}
