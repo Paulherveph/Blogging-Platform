943cf0e25a2d0d26494e420d5e99e42924ae7708
TATIEZE PAUL HERVE <150878556+Paulherveph@users.noreply.github.com>
2024-06-15 07:40:55 +0100

    home.js

--- JS/home.js
+++ JS/home.js
@@ -1,23 +1,30 @@
 import { db } from './firebase.js';
+import { collection, getDocs } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
 const blogSection = document.querySelector('.blogs-section');
 
-db.collection("blogs").get().then((blogs) => {
-  blogs.forEach(blog => {
-    if (blog.id !== decodeURI(location.pathname.split("/").pop())) {
+const fetchBlogs = async () => {
+  try {
+    const blogsCollection = collection(db, "blogs");
+    const blogsSnapshot = await getDocs(blogsCollection);
+    blogsSnapshot.forEach((blog) => {
       createBlog(blog);
-    }
-  });
-}).catch(error => console.error('Error fetching blogs:', error));
+    });
+  } catch (error) {
+    console.error('Error fetching blogs:', error);
+  }
+};
 
 const createBlog = (blog) => {
   let data = blog.data();
   blogSection.innerHTML += `
     <div class="blog-card">
       <img src="${data.bannerImage}" class="blog-image" alt="Blog Banner">
-      <h1 class="blog-title">${data.title.substring(0, 100) +'...'}</h1>
-      <p class="blog-overview">${data.article.substring(0, 200) + '...'}</p>
+      <h1 class="blog-title">${data.title.substring(0, 100)}...</h1>
+      <p class="blog-overview">${data.article.substring(0, 200)}...</p>
       <a href="/${blog.id}" class="btn dark">read</a>
     </div>
   `;
 };
+
+fetchBlogs();
